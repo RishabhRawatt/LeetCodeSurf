e23f91e0f2679d907266674c48aef0fae1b9cdc7
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-05-12 00:14:45 +0530

    Time: 4 ms (35.46%), Space: 6 MB (58.60%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -2,14 +2,22 @@ class Solution {
 public:
     int countVowelStrings(int n) {
         
+        // vector with 5 vowels ans initially store 1 in it
         vector<int> ans(5,1);
+        
+        //for 1 already filled with so start with 2
         for(int i=2;i<=n;i++){
         
+            // form index 3 is second traverese from there
             for(int i=3;i>=0;i--){
+                
+                // ans at i is equal to prev+curr
                 ans[i]+=ans[i+1];
             }
-            
-        }   int ret=0;
+              
+        }   
+        // return now the result by sum all
+            int ret=0;
             for(auto a:ans) ret+=a;
             return ret;
         
