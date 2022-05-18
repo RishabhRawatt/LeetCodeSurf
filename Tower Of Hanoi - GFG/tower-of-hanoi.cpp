7144585af215df6447711ca3869b3f9588c03ce9
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-05-18 21:19:38 +0530

    Added solution - LeetHub

--- Tower Of Hanoi - GFG/tower-of-hanoi.cpp
+++ Tower Of Hanoi - GFG/tower-of-hanoi.cpp
@@ -8,21 +8,26 @@ using namespace std;
 class Solution{
     public:
     // You need to complete this function
-    long long count = 0;
-   long long toh(int N, int from, int to, int aux) {
-       // Your code here
-   if(N==1)
-   {
-       cout<<"move disk 1 from rod "<<from<<" to rod "<<to<<endl;
-       count++;
-       return count;
-   }
-   toh(N-1,from,aux,to);
-   cout<<"move disk "<<N<<" from rod "<<from<<" to rod "<<to<<endl;
-   count ++;
-   toh(N-1,aux,to,from);
-   return count;
-   }
+    long long count=0;
+    // avoid space at the starting of the string in "move disk....."
+    long long toh(int N, int from, int to, int aux) {
+        
+        
+        
+        //base case
+        if(N==1){
+            cout<<"move disk "<<N<<" from rod "<<from<<" to rod "<<to<<endl;
+            count++;
+            return count;
+        }
+        
+        toh(N-1,from,aux,to);
+        cout<<"move disk "<<N<<" from rod "<<from<<" to rod "<<to<<endl;
+        count++;
+        toh(N-1,aux,to,from);
+        return count;
+        
+    }
 
 };
 
