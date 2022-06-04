084b16c4b9b3d122e72cb425f6684cb085dc7de8
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-06-04 22:54:45 +0530

    Time: 14 ms (40.55%), Space: 7.4 MB (55.22%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    vector<vector<string>> res;
-    bool is_valid(vector<string> &board,int row,int col){
-        for(int i=row;i>=0;--i)
-            if(board[i][col]=='Q') return false;
-        for(int i=row,j=col;i>=0 && j>=0;--i,--j){
-            if(board[i][j]=='Q') return false;
-        }
-        for(int i=row,j=col;i>=0 && j<board.size();--i,++j)
-            if(board[i][j]=='Q') return false;
-        return true;
-    }
-    
-    void dfs(vector<string> &board,int row){
-        if(row==board.size()){
-            res.push_back(board);
-            return;
-        }
-        for(int i=0;i<board.size();i++){
-            if(is_valid(board,row,i)){
-                board[row][i]='Q';
-                dfs(board,row+1);
-                board[row][i]='.';
-            }
-        }
-    }
-    vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n,string(n,'.'));
-        dfs(board,0);
-        return res;
-    }
+    vector<vector<string>> ans;
+    bool issafe(int r,int c,vector<string>&v,int n){
+	
+		for(int i=0;i<=r;i++){
+			if(v[i][c]=='Q') return false;
+		}
+		
+		int row=r,col=c;
+		while(row>=0 && col>=0){
+			if(v[row][col]=='Q') return false;
+			row--,col--;
+		}
+		row=r,col=c;
+		while(row>=0 && col<n){
+			if(v[row][col]=='Q') return false;
+			row--,col++;
+		}
+		return true;
+	}
+	void helper(int r,vector<string> &v,int n){
+		 if(r==n){
+			 ans.push_back(v);
+			 return ;
+		 }
+		for(int i=0;i<n;i++){  
+			if(issafe(r,i,v,n)){
+				v[r][i]='Q';
+				helper(r+1,v,n);
+				v[r][i]='.';
+			}
+		}
+	}
+	vector<vector<string>> solveNQueens(int n) {
+		vector<string> v;
+		string s(n,'.');
+		for(int i=0;i<n;i++) v.push_back(s);
+		helper(0,v,n);
+		return ans;
+	}
 };
+    
