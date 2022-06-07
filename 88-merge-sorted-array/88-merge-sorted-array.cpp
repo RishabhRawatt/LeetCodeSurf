d2ca96f3d31f46e551a276ff0cf58153961e7320
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-06-07 23:41:56 +0530

    Time: 4 ms (49.07%), Space: 9.1 MB (30.68%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+         int size=m+n-1;     // total size of a1
+    m--;
+    n--;
+    while(m>=0 && n>=0)
+    {
+        if(nums1[m]>=nums2[n])
+            nums1[size--]=nums1[m--];
+        else
+            nums1[size--]=nums2[n--];
+    }
+    // if after sorting still some elements are left int the array put it 
+    // into a1. 
+    // Check if any element is still left or not
+    if(m>0)
+    {
+        while(m>=0)
+            nums1[size--]=nums1[m--];
+        
+    }
+    else
+    {
+        while(n>=0)
+            nums1[size--]=nums2[n--];
+        
+    }
+
+        
+    }
+};
