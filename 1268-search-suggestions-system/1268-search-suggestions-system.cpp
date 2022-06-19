96c44d3f2b7658da89b9c7d8143f78f5b38ff066
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-06-19 23:28:10 +0530

    Time: 68 ms (77.73%), Space: 26.4 MB (85.68%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
-    vector<vector<string>> suggestedProducts(vector<string>& products, string search) {
-        sort(products.begin(),products.end());
-        int l=0,r=products.size()-1;
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        
+        //to short so all strings will in order
+        sort(products.begin(), products.end());
+        
+        //two pointer for checking
+        int l=0;
+        int r=products.size()-1;
+        
         vector<vector<string>> ans;
-        for(int i=0;i<search.size();i++)
-        {
-            char ch=search[i];
+        
+        for(int i=0;i<searchWord.size();i++){
+            
             vector<string> temp;
-            while(l<=r && products[l][i]!=ch)
+            
+            //check first one 
+            while(l<=r && products[l][i] != searchWord[i])
                 l++;
-            while(l<=r && products[r][i]!=ch)
+            
+            //check from last
+            while(l<=r && products[r][i] != searchWord[i])
                 r--;
-            for(int j=l;j<l+min(3,r-l+1);j++)
+            
+            // if it matches then just add only top three string values
+            for(int j=l;j<l+min(3,r-l+1);j++){
+                
                 temp.push_back(products[j]);
+            }
+            
             ans.push_back(temp);
+            
         }
         return ans;
     }
