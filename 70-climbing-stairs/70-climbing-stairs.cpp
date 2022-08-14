dc22789201bc4fb413dc05b16525f9aa5a5c601d
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-08-15 00:14:09 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (56.91%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+  int dp[50]={0};
+    
+    
+    int climbStairs(int n) {
+       
+    
+         
+        if(n==1 || n==0){
+            return 1;
+            
+        }
+            if(dp[n])
+                return dp[n];
+        
+            
+       return dp[n]=climbStairs(n-1) + climbStairs(n-2);
+    }
+};
