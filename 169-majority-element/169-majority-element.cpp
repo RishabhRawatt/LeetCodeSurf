7af079ae1802131e2f50a504ba951ad6e38f9824
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-08-14 19:33:33 +0530

    Time: 20 ms (88.86%), Space: 19.4 MB (92.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
-        map<int,int> freq;
+        //by moore voting algo
         
-        for(int i=0;i<nums.size();i++){
+        int count=0;
+        int element=0;
+        
+        for(int i =0;i<nums.size();i++){
             
-            if(freq.find(nums[i])==freq.end()){
-                freq[nums[i]]=1;
-            }
-            else{
-                freq[nums[i]]++;
-            }
+            if(count==0)
+                element=nums[i];
+            
+            if(element==nums[i])
+                count++;
+            else
+                count--;
         }
+        return element;
         
-        for(auto it : freq){
-            if(it.second > nums.size()/2)
-                return it.first;
-        }
-        return -1;
     }
 };
