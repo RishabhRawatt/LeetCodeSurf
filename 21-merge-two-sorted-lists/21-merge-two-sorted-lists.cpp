8e31616dfab05ef06714713cf9a990f7b18cddf1
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-09-13 23:19:28 +0530

    Time: 20 ms (14.58%), Space: 14.7 MB (97.81%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,62 +10,30 @@
  */
 class Solution {
 public:
-   
-    ListNode* solve(ListNode* list1, ListNode* list2){
-        
-        if(list1->next==NULL){
-            list1->next=list2;
-            return list1;
-        }
-        
-        ListNode* curr1=list1;
-        ListNode* next1=list1->next;
-        
-        ListNode* curr2=list2;
-        ListNode* next2=list2->next;
-        
-        while( next1 != NULL && curr2 !=NULL ){
-            
-            if(curr1->val <= curr2->val && curr2->val <= next1->val){
-                
-                //attach the node to the main list
-                curr1->next=curr2;
-                next2=curr2->next;
-                curr2->next=next1;
-                
-                //to increment the pointers
-                curr1=curr2;
-                curr2=next2;
-                }
-            else{
-                curr1=curr1->next;
-                next1=next1->next;
-                
-                //if 1st node ends insert all remain to 1st
-                if(next1==NULL){
-                    curr1->next=curr2;
-                    return list1;
-                }
-            }
-            
-        }            
-        return list1;
-        
-    }
-    
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         
-        if(!list1)
+        //base case
+        if(list1 ==NULL)
             return list2;
-        if(!list2)
+        if(list2 ==NULL)
             return list1;
         
-        if(list1->val <= list2->val)
-           return solve(list1,list2);
-        else
-            return solve(list2,list1);
+        if(list1->val > list2->val) swap(list1,list2);
         
-        // return list1;
+        ListNode * result=list1;
         
+        while(list1 !=NULL && list2 !=NULL){
+            
+            ListNode *temp=NULL;
+            
+            while(list1 !=NULL && list1->val <= list2->val){
+                
+                temp=list1;
+                list1=list1->next;
+            }
+            temp->next=list2;
+            swap(list1,list2);
+        }
+        return result;
     }
 };
