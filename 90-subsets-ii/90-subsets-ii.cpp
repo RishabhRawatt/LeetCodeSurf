e14f8ffab2d98e250fb57fb953d8428dbbd4d024
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-09-21 23:37:22 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (53.86%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    void helper(int index,vector<int>nums,vector<int>&ds,vector<vector<int>> & ans){
+        
+        
+        //base case
+        ans.push_back(ds);
+        
+        for(int i=index;i<nums.size();i++){
+            
+            if(i !=index && nums[i] == nums[i-1]) continue;
+            
+            ds.push_back(nums[i]);
+            
+            helper(i+1,nums,ds,ans);
+            ds.pop_back();
+        }
+        
+        
+        
+        
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+        vector<int> ds;
+        vector<vector<int>> ans;
+        
+        sort(nums.begin(),nums.end());
+        helper(0,nums,ds,ans);
+        
+        return ans;
+    }
+};
