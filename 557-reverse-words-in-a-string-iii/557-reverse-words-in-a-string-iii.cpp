3a92fe509497aae29d2e18454763c1221d100928
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-09-23 00:05:39 +0530

    Time: 11 ms (96.48%), Space: 9.5 MB (98.60%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -9,7 +9,13 @@ public:
                     r++;
                 
             }
-            reverse(s.begin()+l,s.begin()+r);
+            // reverse(s.begin()+l,s.begin()+r);
+            int i=l,j=r-1;
+            while(i<=j){
+                swap(s[i],s[j]);
+                i++;
+                j--;
+            }
             l=r+1;
             r=l;
         }
