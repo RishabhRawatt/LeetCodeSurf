c5e7276b5d5d8ca01de78c4be13f614f7c43398b
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-09-23 23:48:00 +0530

    Time: 7 ms (43.10%), Space: 8 MB (13.26%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,31 +1,54 @@
 class Solution {
 public:
     
-    int helper(int i,int j,vector<vector<int>>&obstacleGrid,vector<vector<int>> &dp){
+//     int helper(int i,int j,vector<vector<int>>&obstacleGrid,vector<vector<int>> &dp){
         
-        //basecase
-        if(i>=0 && j>=0 && obstacleGrid[i][j]==1)
-            return 0;
-        if(i==0 && j==0)
-            return 1;
-        if(i<0 || j<0)
-            return 0;
-        if(dp[i][j]!= -1)
-            return dp[i][j];
-        int up=helper(i-1,j,obstacleGrid,dp);
-        int left=helper(i,j-1,obstacleGrid,dp);
+//         //basecase
+//         if(i>=0 && j>=0 && obstacleGrid[i][j]==1)
+//             return 0;
+//         if(i==0 && j==0)
+//             return 1;
+//         if(i<0 || j<0)
+//             return 0;
+//         if(dp[i][j]!= -1)
+//             return dp[i][j];
+//         int up=helper(i-1,j,obstacleGrid,dp);
+//         int left=helper(i,j-1,obstacleGrid,dp);
         
-        return dp[i][j]=up+left;
+//         return dp[i][j]=up+left;
         
-    }
+//     }
+    
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         
-        int n=obstacleGrid.size();
-        int m=obstacleGrid[0].size();
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
 
         //initlize dp
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-
-        return helper(n-1,m-1,obstacleGrid,dp);
+       vector<vector<int> > dp( m , vector<int> (n, 0));  
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+                
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
