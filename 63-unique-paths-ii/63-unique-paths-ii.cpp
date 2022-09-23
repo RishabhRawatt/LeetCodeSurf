670dc698170fa3d5518af94b1a96a1ce427a98f2
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-09-23 23:59:25 +0530

    Time: 6 ms (50.13%), Space: 7.7 MB (67.05%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -24,31 +24,36 @@ public:
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
 
-        //initlize dp
-       vector<vector<int> > dp( m , vector<int> (n, 0));  
+        
+        //row
+        vector<int> pdp(n,0);
         
         for(int i=0;i<m;i++){
+            
+            vector<int>cdp(n,0);
+            
             for(int j=0;j<n;j++){
                 
                 if(obstacleGrid[i][j]==1){
-                    dp[i][j]=0;
+                    cdp[j]=0;
                     continue;
                 }
                 
                 if(i==0 && j==0){
-                    dp[i][j]=1;
+                    cdp[j]=1;
                     continue;
                 }
                 else{
                     int up=0;
                     int left=0;
-                    if(i>0) up=dp[i-1][j];
-                    if(j>0) left=dp[i][j-1];
+                    if(i>0) up=pdp[j];
+                    if(j>0) left=cdp[j-1];
                     
-                    dp[i][j]=up+left;
+                    cdp[j]=up+left;
                 }
             }
+            pdp=cdp;
         }
-        return dp[m-1][n-1];
+        return pdp[n-1];
     }
 };
