285ec3ff1f640db17753817c50d09df644234877
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2022-09-28 23:49:44 +0530

    Time: 4 ms (90.52%), Space: 7 MB (94.55%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,42 +1,55 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
-    bool issafe(int r,int c,vector<string>&v,int n){
+//     vector<vector<string>> ans;
+//     bool issafe(int r,int c,vector<string>&v,int n){
 	
-		for(int i=0;i<=r;i++){
-			if(v[i][c]=='Q') return false;
-		}
+// 		for(int i=0;i<=r;i++){
+// 			if(v[i][c]=='Q') return false;
+// 		}
 		
-		int row=r,col=c;
-		while(row>=0 && col>=0){
-			if(v[row][col]=='Q') return false;
-			row--,col--;
-		}
-		row=r,col=c;
-		while(row>=0 && col<n){
-			if(v[row][col]=='Q') return false;
-			row--,col++;
-		}
-		return true;
-	}
-	void helper(int r,vector<string> &v,int n){
-		 if(r==n){
-			 ans.push_back(v);
-			 return ;
+// 		int row=r,col=c;
+// 		while(row>=0 && col>=0){
+// 			if(v[row][col]=='Q') return false;
+// 			row--,col--;
+// 		}
+// 		row=r,col=c;
+// 		while(row>=0 && col<n){
+// 			if(v[row][col]=='Q') return false;
+// 			row--,col++;
+// 		}
+// 		return true;
+// 	}
+	void helper(int col,vector<string> &board,vector<vector<string>> &ans,vector<int> &leftrow,vector<int> &lowerdiagonal,vector<int> &upperdiagonal,int n){
+		 if(col==n){
+			 ans.push_back(board);
+			 return;
 		 }
-		for(int i=0;i<n;i++){  
-			if(issafe(r,i,v,n)){
-				v[r][i]='Q';
-				helper(r+1,v,n);
-				v[r][i]='.';
+        
+		for(int row=0;row<n;row++){  
+			if(leftrow[row]==0 && lowerdiagonal[row+col]==0 && upperdiagonal[n-1+col-row]==0){
+				board[row][col]='Q';
+                leftrow[row]=1;
+                lowerdiagonal[row+col]=1;
+                upperdiagonal[n-1+col-row]=1;
+				helper(col+1,board,ans,leftrow,lowerdiagonal,upperdiagonal,n);
+				board[row][col]='.';
+                leftrow[row]=0;
+                lowerdiagonal[row+col]=0;
+                upperdiagonal[n-1+col-row]=0;
+				
 			}
 		}
 	}
 	vector<vector<string>> solveNQueens(int n) {
-		vector<string> v;
+        vector<int>leftrow(n,0);
+        vector<int>lowerdiagonal(2*n-1,0);
+        vector<int>upperdiagonal(2*n-1,0);
+        
+        vector<vector<string>>ans;
+		vector<string> board;
 		string s(n,'.');
-		for(int i=0;i<n;i++) v.push_back(s);
-		helper(0,v,n);
+		for(int i=0;i<n;i++) board.push_back(s);
+		helper(0,board,ans,leftrow,lowerdiagonal,upperdiagonal,n);
 		return ans;
 	}
 };
