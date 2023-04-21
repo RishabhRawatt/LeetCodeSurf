0394b6910699e86ce84c3ebacf45ea0acbf3f2a7
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2023-04-21 23:53:47 +0530

    Time: 236 ms (39.62%), Space: 12.8 MB (72.64%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
-  int MOD = 1000000007;
-  vector<vector<vector<int>>>dp;
-  int solve(vector<int>&group, vector<int>&profit, int minProfit, int maxPeople, int index)
-  {
-      if (maxPeople < 0) return 0; //means that we took more people than we were allowed
-      if (index == group.size())
-      {
+    
+    
+    //approach 0/1 knapsack
+    int N;
+    int t[101][101][101];
+    const int MOD =1e9+7;
+    
+    int solve(int i,int p,int people,int &minProfit, vector<int>& group, vector<int>& profit){
 
-          if (minProfit > 0) return 0; //means that still there is some 'minProfit' left to make
-                                       //but now the list of crimes is over :(..so no way to make profit
-          else return 1; //either we got profit = minProfit or profit > minProfit
-      }
-      
-      if (minProfit < 0) minProfit = 0; //***CHANGED*****
-      if (dp[minProfit][maxPeople][index] != -1) return dp[minProfit][maxPeople][index];
-      //================================================================================================
-      int pickWays = solve(group, profit, minProfit - profit[index], maxPeople - group[index], index + 1);
-      int notPickWays = solve(group, profit, minProfit, maxPeople, index + 1);
-      //==================================================================================================
-      return dp[minProfit][maxPeople][index] = (pickWays + (long long)notPickWays) % MOD;
-  }
-  int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) 
-  {
-      int sz = group.size();
-      dp = vector<vector<vector<int>>>(minProfit + 1, vector<vector<int>>(n + 1, vector<int>(sz, -1)));
-      int ans = solve(group, profit, minProfit, n, 0);
-      return ans;
-  }
+        //base cases
+        if(people >N) return 0;
+        
+        if(i==group.size()){
+            if(p>=minProfit)
+                return t[i][p][people]=1;
+            
+            return t[i][p][people]=0;
+        }
+        
+        if(t[i][p][people]!= -1){
+            return t[i][p][people];
+        }
+        
+        long long not_taken=solve(i+1,p,people,minProfit,group,profit)%MOD;
+        long long taken=solve(i+1,min(p+profit[i],minProfit),people+group[i],minProfit,group,profit)%MOD;
+        
+        return t[i][p][people] = (not_taken%MOD + taken%MOD)%MOD;
+    }
+    
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+     
+        N=n;
+        memset(t,-1,sizeof(t));
+        return solve(0,0,0,minProfit,group,profit);
+    }
 };
