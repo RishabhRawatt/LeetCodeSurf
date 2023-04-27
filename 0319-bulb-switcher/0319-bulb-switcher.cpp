6c730d1bce07cb4e0557df5c4cbbcfaaaa9d0d09
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2023-04-27 23:04:30 +0530

    Time: 3 ms (45.34%), Space: 5.9 MB (63.99%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        
+        //intution 
+        // only perfect square nos will be remain on (because they have odd no of root pairs so if the total root pairs are odd then means it will left to on condition )
+        
+        
+       return sqrt(n);
+    }
+};
