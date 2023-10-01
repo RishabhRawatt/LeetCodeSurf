dbd7499d027359a1938ea51b14563e2a6b2f0055
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2023-10-01 20:23:13 +0530

    Time: 20 ms (8.87%), Space: 20.6 MB (12.93%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,24 +1,31 @@
 class Solution {
-public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        
-        int count=0;
-        for(int i=0;i<flowerbed.size();i++){
-            
-            if(flowerbed[i]==0){
-                   bool checkleft = (i == 0) ? true : (flowerbed[i - 1] == 0);
-bool checkright = (i == flowerbed.size() - 1) ? true : (flowerbed[i + 1] == 0);
+  public: bool canPlaceFlowers(vector < int > & flowerbed, int n) {
 
-            
-            
-            if(checkleft && checkright){
-                flowerbed[i]=1;
-                count++;
-            }
-                }
+    //make a count variable to store can plant space
+    int count = 0;
+    int size = flowerbed.size();
+
+    for (int i = 0; i < size; i++) {
+
+      if (flowerbed[i] == 0) {
+
+        //now check left and right
+        bool isLeft = (i == 0) || flowerbed[i - 1] == 0;
+        bool isRight = (i == size - 1) || flowerbed[i + 1] == 0;
+
+         //check left right both should true
+        if (isLeft && isRight) {
+          flowerbed[i] = 1;
+          count++;
+            //this is for if we reach to n then no need to proceed further extra 
+          if (count >= n) {
+            return true;
+          }
         }
-        
-        return count>=n;
-        
+
+      }
+
     }
+    return count >= n;
+  }
 };
