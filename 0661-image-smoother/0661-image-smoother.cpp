7dfd618241dc8ed76d4e4f711d3aea84bafc8755
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2023-12-19 13:36:22 +0530

    Time: 44 ms (55.08%), Space: 22 MB (59.38%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.cpp
+++ 0661-image-smoother/0661-image-smoother.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
-        
-        int n=img.size();
-        int m=img[0].size();
-        
-        vector<vector<int>>direction{
-            {-1,-1},{-1,0},{-1,1},
-            {0,-1},{0,0},{0,1},
-            {1,-1},{1,0},{1,1}
-        };
-        
-        vector<vector<int>> result(n,vector<int>(m,0));
-        
-        
-        for(int i=0;i<n;i++){
-            
-            for(int j=0;j<m;j++){
-                
-                //find sum of neighbor
-                int sum=0;
-                int count=0;
-                
-                for(auto it:direction){
-                    int x=i+it[0]; //get xcordinate
-                    int y=j+it[1]; //get y
-                    
-                    //check valid
-                    if(x >= 0 && x < n && y >= 0 && y < m){
-                        sum+=img[x][y];
-                        count++;
+    
+      vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+          
+        int m=img.size(),n=img[0].size(),cnt,sum;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                cnt=0;sum=0;
+                for(int ii=-1;ii<2;ii++){
+                    for(int jj=-1;jj<2;jj++){
+                        
+                        if((i+ii)>=0 && (j+jj)>=0 && (i+ii)<m && (j+jj)<n){
+                            sum+=img[i+ii][j+jj] % 256;
+                            cnt++;
+                        }
                     }
-                    
                 }
-                //get avg
-                result[i][j]=sum/count;
+                img[i][j]=img[i][j] ^ (sum/cnt) << 8;
             }
         }
-        return result;
+          
+          for(int i=0;i<m;i++){
+              for(int j=0;j<n;j++){
+                    img[i][j]=img[i][j] >> 8;
+              }
+          }
+        return img;
     }
 };
