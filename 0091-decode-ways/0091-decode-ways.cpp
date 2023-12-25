489e3cdad1d8f60c84360ee4b836fcb2c854c09b
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2023-12-25 15:29:59 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (65.44%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    
-    //top down
-    
-    int t[101];
-    
-    int solve(int i,string &s,int &n){
-        
-        if(t[i]!=-1)
-            return t[i];
-        
-        if(i==n) return t[i]=1; //only one valid
-        if(s[i]=='0') return t[i]=0; // this will not valid dont consider it
-        
-        int result = solve(i+1,s,n); // considering one one
-        
-        if( i+1 < n ){
-            //that means if i is one then we dont care for second num (10-19)
-            //but i is 2 then we only allow next to that i must be <= 6 (20-26)
-            if(s[i]=='1' || (s[i]=='2' && s[i+1]<='6'))
-                //add in solve
-                result+=solve(i+2,s,n);
-        }
-        return t[i]=result;
-    }
+    //bottom up
     
     int numDecodings(string s) {
-        int n=s.length();
         
-        memset(t,-1,sizeof(t));
+        int n=s.size();
         
-        return solve(0,s,n);
+        vector<int> t(n+1,0);
+        // t[i]= ways to decode string s from index i to n
+        t[n]=1;
+        
+        for(int i=n-1 ;i >=0 ;i--){
+            
+            if(s[i]=='0') t[i]=0;
+            else{
+                t[i]=t[i+1];  //same as solve(i+1)
+                
+                if(i+1 < n){
+                    if(s[i]=='1' || s[i]== '2' && s[i+1] <= '6')
+                        t[i]+=t[i+2];  //same as solve(i+2)
+                }
+            }
+        }
+        
+        return t[0]; //s[0 to n ] full string decode ways
     }
 };
