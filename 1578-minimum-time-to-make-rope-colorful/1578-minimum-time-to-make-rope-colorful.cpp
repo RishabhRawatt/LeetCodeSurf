2cc560bd74bdfe76fd04bd31950835e005eff5e9
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2023-12-27 13:39:39 +0530

    Time: 114 ms (73.03%), Space: 95.7 MB (83.31%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    //two pinter
     int minCost(string colors, vector<int>& neededTime) {
         
         int result=0;
+        int left=neededTime[0];
         
-        int l=0;
-        
-        for(int r=1;r<colors.size();r++){
+        for(int i=1;i<colors.size();i++){
             
-            if(colors[l] == colors[r]){
-                
-                if(neededTime[l] < neededTime[r]){
-                    result+=neededTime[l];
-                    l=r; // we didnt do ++ cause contain of holes [a,b,'',a ,c]
-                }
-                else
-                    result+=neededTime[r];    
+            if(colors[i] != colors[i-1]){
+                left=0;
             }
-            else
-            {
-                l=r;
-            }
+            
+            int right=neededTime[i];
+            result+=min(left,right);
+            left=max(left,right);
+            
             
         }
         return result;
+        
     }
 };
