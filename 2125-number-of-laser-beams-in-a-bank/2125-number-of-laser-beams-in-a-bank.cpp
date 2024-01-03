240848e6c22db2f454f7df1db4994f8598d51d94
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2024-01-03 15:05:30 +0530

    Time: 104 ms (24.97%), Space: 23.3 MB (96.49%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -3,21 +3,23 @@ public:
     int numberOfBeams(vector<string>& bank) {
         int prev=0;
         
-        for(int i=0;i<bank[0].size();i++){
-            if(bank[0][i]=='1') prev++;
-        }
+//         for(int i=0;i<bank[0].size();i++){
+//             if(bank[0][i]=='1') prev++;
+//         }
         
         int totalLazer=0;
         
-        for(int i=1;i<bank.size();i++){
+        for(int i=0;i<bank.size();i++){
             int currLazer=0;
             for(int j=0;j<bank[i].size();j++){
                 if(bank[i][j]=='1')
                     currLazer++;
             }
-            if(currLazer!=0){
+            if(prev==0){
+                prev=currLazer;
+            }
+            else if(currLazer!=0){
                 totalLazer+=currLazer*prev;
-                cout<<totalLazer<<" ---";
                 prev=currLazer;
                 }
         }
