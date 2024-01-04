04708ac61096289c31625fad86a0b6105bd29d12
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2024-01-04 21:43:15 +0530

    Time: 248 ms (89.29%), Space: 109.6 MB (89.87%) - LeetHub

--- 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.cpp
+++ 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.cpp
@@ -8,22 +8,20 @@ public:
         int n=nums.size();
         
         for(int i=0;i<n;i++){
-            
-            for(int j=i;j<n;j++){
-                
-                string temp; 
-                int count=0;
-                for(int k=i;k<=j;k++){
-                    temp.push_back(nums[k]);
+            string temp; 
+            int count=0;
+            for(int j=i;j>=0;j--){
+                temp.push_back(nums[j]);
                     //check its divisible
-                    if(nums[k] % p == 0) count++;
+                    if(nums[j] % p == 0) count++;
                     //if count exceed don't consider
-                    }
-                    if(count <= k)
+                    
+                     if(count <= k)
                         st.insert(temp);
-                
+                else break;
             }
-        }
+            }
+        
         return st.size();
         
     }
