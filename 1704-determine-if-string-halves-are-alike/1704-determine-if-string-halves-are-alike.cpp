577798bfa9a5ebbd4145828b966774a780509b9e
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2024-01-12 13:33:43 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (46.92%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -2,26 +2,23 @@ class Solution {
 public:
     bool halvesAreAlike(string s) {
         
-        char arr[10]={'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
         
         int n=s.size();
         
-        int first=0;
-        int second=0;
+        int count=0;
         int half=n/2;
         for(int i=0;i<n;i++){
             if(i<half){
-                for(int j=0;j<10;j++){
-                    if(arr[j]==s[i])
-                        first++;
+                if(vowels.find(s[i])!=vowels.end()){
+                        count++;
                 }
             }else{
-                for(int j=0;j<10;j++){
-                    if(arr[j]==s[i])
-                        second++;
+                if(vowels.find(s[i])!=vowels.end()){
+                        count--;
                 }
             }
         }
-        return first==second;
+        return count==0;
     }
 };
