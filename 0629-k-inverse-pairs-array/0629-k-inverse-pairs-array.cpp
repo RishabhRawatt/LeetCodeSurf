273d068a29d8e9ec48e50d9b4631f0db1e6d2cfa
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2024-01-27 16:32:53 +0530

    Time: 2405 ms (7.14%), Space: 11.4 MB (43.41%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
+    //T.C --> O(n*k*n)
+    //S.c -> O(n*k)
     
-    //T.C  O(k^n) --- recursion
-    //T.C  O(n*k*(n-1)) ---- memeoization
+    //bottom up
     
     int MOD=1e9+7;
-    int dp[1001][1001];
-    //recursive
     
-    int solve(int n,int k){
-        
-        int ans=0;
-        
-        //base case
-        if(n==0) return 0;
-        if(k==0) return 1; //sorted array have 0 inversions
-        
-        //dp
-        if(dp[n][k] != -1) return dp[n][k];
-        
-        // min(k,n-1) because max inversion possible is
-        //n-1 so k must me less than or equal to that
-        for(int inv=0;inv<=min(k,n-1);inv++){
-            
-            //we have inv inversions need k-inv more
-            ans=( ans%MOD +solve(n-1,k-inv)%MOD )%MOD;
-        }
-        return dp[n][k]=ans;
-    }
     int kInversePairs(int n, int k) {
         
-        memset(dp,-1,sizeof(dp));
-        return solve(n,k);
+        //dp[i][j]--> total ways of having (1 to i) and excact j inversions
+        
+        int dp[n+1][k+1];
+        memset(dp,0,sizeof(dp));
+        
+     
+        
+        //same for k=0; put 1
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        
+        for(int i=1;i<=n;i++){
+            
+            for(int j=1;j<=k;j++){
+                
+                //same memo logic
+                for(int inv=0;inv<=min(j,i-1);inv++){
+                    dp[i][j] = (dp[i][j]%MOD + dp[i-1][j-inv]%MOD) %MOD;
+                }
+            }
+        }
+        return dp[n][k];
+        
     }
 };
