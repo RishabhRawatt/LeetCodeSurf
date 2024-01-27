003932186c8bd913823c9accaa3eef6ae21d9940
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2024-01-27 16:08:58 +0530

    Time: 2583 ms (6.59%), Space: 11.6 MB (42.31%) - LeetHub

---
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    int MOD=1e9+7;
+    int dp[1001][1001];
+    //recursive
+    
+    int solve(int n,int k){
+        
+        int ans=0;
+        
+        //base case
+        if(n==0) return 0;
+        if(k==0) return 1; //sorted array have 0 inversions
+        
+        //dp
+        if(dp[n][k] != -1) return dp[n][k];
+        
+        // min(k,n-1) because max inversion possible is
+        //n-1 so k must me less than or equal to that
+        for(int inv=0;inv<=min(k,n-1);inv++){
+            
+            //we have inv inversions need k-inv more
+            ans=( ans%MOD +solve(n-1,k-inv)%MOD )%MOD;
+        }
+        return dp[n][k]=ans;
+    }
+    int kInversePairs(int n, int k) {
+        
+        memset(dp,-1,sizeof(dp));
+        return solve(n,k);
+    }
+};
