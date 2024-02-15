10cb186432dd00554f83e0064b49387f546e6058
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2024-02-15 23:56:19 +0530

    Time: 146 ms (51.90%), Space: 83.3 MB (36.11%) - LeetHub

--- 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.cpp
+++ 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
+    
+    //t.c O(n)
     long long largestPerimeter(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-
-        long long perimeter = 0, sides = n;
         
-        // Consider the largest polygon with all the sides at first
-        for(auto it: nums) perimeter += it;
-
-        // start from the largest polygon and remove 
-        // the largest side till a valid polygon can be formed. 
-        // i.e. sum of all other sides <= longest side
-        // i.e. cur. perimeter - largest <= largest
-        // i.e. cur. perimeter <= 2*largest
-        for(int i=n-1; i>=2; i--){
-            if(perimeter <= 2*nums[i]){
-                perimeter -= nums[i];
-                sides--;
+        sort(nums.begin(),nums.end());
+        
+        long long totalsum=0;
+        
+        long long perimeter=0;
+        
+        for(auto it:nums){
+            if(it < totalsum){
+                perimeter=totalsum+it;
             }
-            else break;
+            totalsum+=it;
         }
-
-        // if sides is 2 then return -1 
-        if(sides == 2) return -1;
-        // else return the largest perimeter
-        return perimeter;
+        
+        return perimeter==0 ? -1 :perimeter ;
     }
 };
