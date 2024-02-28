0358deb3a2eee1efa5bd2445eeb9635e90905807
Rishabh Rawat <63269542+RishabhRawatt@users.noreply.github.com>
2024-02-28 16:18:32 +0530

    Time: 7 ms (79.35%), Space: 20.2 MB (63.53%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -12,20 +12,18 @@
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
-        queue<TreeNode*>q;
-
+        
+        queue<TreeNode *>q;
         q.push(root);
-        TreeNode *a=NULL;
-        while(!q.empty()){
-            a = q.front();
-            q.pop();
+        TreeNode * a;
 
-            if(a->right){
-                q.push(a->right);
-            }
-            if(a->left){
-                q.push(a->left);
-            }
+        while(!q.empty()){
+            
+            a=q.front();
+            q.pop();
+            
+            if(a->right) q.push(a->right);
+            if(a->left) q.push(a->left);
         }
         return a->val;
     }
